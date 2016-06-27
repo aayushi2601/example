ff01164221ef4c797463bef451958d1e5c0dc282
root <aayushi.garg@tothenew.com>
2016-06-27 16:09:18 +0530

    user name add

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php include_once('connect.inc'); ?>
 <?php include_once('header.inc'); ?>
         <div id="section">
-            <h1>London</h1>
+            <h1><?php echo $_SESSION['uname']; ?></h1>
             
         </div>
 <?php include_once('footer.inc'); ?>
