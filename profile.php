3d2552fa69881bd5e5dadf7ca9d34f7ce78b06f1
aayushi2601 <aayushi.garg@tothenew.com>
2016-06-29 11:24:29 +0530

    changes

--- profile.php
+++ profile.php
@@ -2,6 +2,11 @@
 <?php include_once('header.inc'); ?>
         <div id="section">
             <h1><?php echo $_SESSION['uname']; ?></h1>
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+</form>
             
         </div>
 <?php include_once('footer.inc'); ?>
